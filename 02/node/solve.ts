b280531fdff130d9052554814e8b554bbe70154a
drrefactor <drrefactor@gmail.com>
2024-12-03 10:51:20 +0100

    Cleanup task 02 part 1 node

--- 02/node/solve.ts
+++ 02/node/solve.ts
@@ -10,14 +10,8 @@ function isDiffSafe(diff: number) {
   return a >= MIN_DIFF && a <= MAX_DIFF;
 }
 
-const delimeter = new RegExp(/\s+/);
-const safeReports = file.split("\n").reduce((r, line) => {
-  if (!line.length) {
-    return r;
-  }
-  const report = line.split(delimeter).map((s) => +s);
-
-  let isRising: boolean | undefined;
+function isReportSafe(report: number[]): boolean {
+  let isInreasing: boolean | undefined;
   let previous: number | undefined;
   for (const element of report) {
     if (previous === undefined) {
@@ -26,19 +20,29 @@ const safeReports = file.split("\n").reduce((r, line) => {
     }
     const diff = element - previous;
     if (!isDiffSafe(diff)) {
-      return r;
+      return false;
     }
-    const isLocallyRising = diff > 0;
-    isRising ??= isLocallyRising;
+    const isLocallyIncreasing = diff > 0;
+    isInreasing ??= isLocallyIncreasing;
 
-    if (isRising !== isLocallyRising) {
-      return r;
+    if (isInreasing !== isLocallyIncreasing) {
+      return false;
     }
 
     previous = element;
   }
 
-  return r + 1;
+  return true;
+}
+
+const delimeter = new RegExp(/\s+/);
+const safeReports = file.split("\n").reduce((r, line) => {
+  if (!line.length) {
+    return r;
+  }
+  const report = line.split(delimeter).map((s) => +s);
+
+  return isReportSafe(report) ? r + 1 : r;
 }, 0);
 
 console.log("safe reports: ", safeReports);
